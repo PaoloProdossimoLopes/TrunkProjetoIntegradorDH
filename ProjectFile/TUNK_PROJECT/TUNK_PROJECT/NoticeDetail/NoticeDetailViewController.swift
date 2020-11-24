b4c2317c171451305b15911116b7c9de3f995bf9
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2020-11-24 13:01:04 -0300

    add NewsPageDetail

---
+++ ProjectFile/TUNK_PROJECT/TUNK_PROJECT/NoticeDetail/NoticeDetailViewController.swift
@@ -0,0 +1,34 @@
+//
+//  NoticeDetailViewController.swift
+//  TUNK_PROJECT
+//
+//  Created by Paolo Prodossimo Lopes on 24/11/20.
+//  Copyright © 2020 Paolo Prodossimo Lopes. All rights reserved.
+//
+
+import UIKit
+
+class NoticeDetailViewController: UIViewController {
+
+    @IBAction func closeNoticeDetail(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
